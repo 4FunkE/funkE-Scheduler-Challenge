24e890de8d734e05d98d5599b537df03c163e3e8
4FunkE <4FunkEcodes@gmail.com>
2023-05-18 21:57:48 -0400

    organized js code and psuedo

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-var currentDate = dayjs();
-    $("#currentDay").text(currentDate.format("MMM D, YYYY"));
-
+//function for save button to save, set, and get
 $(function () {
     // listener for click events on the save button.
     var savebtn = $(".time-block");
@@ -21,24 +15,27 @@ $(function () {
             localStorage.setItem(time, value);
     });
 
-  
-
-
-
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    
+
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+  
+
+
+    //code to display the current date in the header of the page.
+    var currentDate = dayjs();
+      $("#currentDay").text(currentDate.format("MMM D, YYYY"));
+
   });
 
-  //add getItem for every hour, to render todo to page
+  //getItem for every hour, to render todo to page
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
