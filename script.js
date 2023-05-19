f5f6cd77bc981443554330ab49cc5c100b33fbfc
4FunkE <4FunkEcodes@gmail.com>
2023-05-18 21:52:13 -0400

    added buisness hours with localstorage code

--- script.js
+++ script.js
@@ -2,51 +2,26 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    var savebtn = $(".time-block");
-    var todos = [];
-    var hour9 = $("#hour-9");
-    var hour10 = $("#hour-10");
-    
-    var allToDos = {
-      hour9: hour9.value,
-      hour10: hour10.value,
-    }
-
-    var currentDate = dayjs();
+var currentDate = dayjs();
     $("#currentDay").text(currentDate.format("MMM D, YYYY"));
+
+$(function () {
+    // listener for click events on the save button.
+    var savebtn = $(".time-block");
 
     savebtn.on("click", "button", function() {
         console.log("clicked!")
-            var value = $(this).siblings(".description").val();
-            var time = $(this).parent().attr("id");
+            var value = $(this).siblings(".description").val();//DOM traversal
+            var time = $(this).parent().attr("id");//DOM traversal
             
             console.log(value);
             console.log(time);
             
             // save in localStorage
             localStorage.setItem(time, value);
-            renderToDoItem();
     });
 
-    //need to save on reload!!!
-    function renderToDoItem() {
-        // Get stored todos from localStorage
-        var lastUserInput = localStorage.getItem("hour-9");
-        console.log(lastUserInput)
-      
-        // If todos were retrieved from localStorage, update the todos array to it
-        if (lastUserInput !== null) {
-            todos = lastUserInput;
-          }
-      };
-    
+  
 
 
 
@@ -62,3 +37,14 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
+
+  //add getItem for every hour, to render todo to page
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-1 .description').val(localStorage.getItem('hour-1'));
+  $('#hour-2 .description').val(localStorage.getItem('hour-2'));
+  $('#hour-3 .description').val(localStorage.getItem('hour-3'));
+  $('#hour-4 .description').val(localStorage.getItem('hour-4'));
+  $('#hour-5 .description').val(localStorage.getItem('hour-5'));
