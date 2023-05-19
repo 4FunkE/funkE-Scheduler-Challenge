473eb3833cd4d69e487c1ff2cd08b20c0f4532d1
4FunkE <4FunkEcodes@gmail.com>
2023-05-18 22:28:55 -0400

    trying to update time

--- script.js
+++ script.js
@@ -20,7 +20,14 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    
+    function updateTime() {
+      //current hour
+      var currentHour = dayjs().hour();
+
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      })
+    }
 
 
     // TODO: Add code to get any user input that was saved in localStorage and set
